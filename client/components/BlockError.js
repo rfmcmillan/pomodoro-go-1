e68988aab27ed60f3e661a3ea40c55d5e10c537a
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-09-21 16:31:20 -0400

    added quotes to blockError page

--- client/components/BlockError.js
+++ client/components/BlockError.js
@@ -1,126 +1,63 @@
-import React, { useState } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Typography,
-  Radio,
-  RadioGroup,
-  FormControlLabel,
-  FormControl,
-  FormLabel,
-} from '@material-ui/core';
-import MuiAlert from '@material-ui/lab/Alert';
-import { updateBlocking } from '../store/blockSites';
-
-const Alert = (props) => {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-};
+import { Grid, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  uhoh: {
-    width: '60%',
-    margin: '20px auto 20px auto',
-    border: '1px solid #a83942',
-    borderRadius: 10,
-    padding: 20,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    boxShadow: '0 5px 10px 0px #ccb8b8',
-    backgroundColor: 'white',
-  },
-  form: {
-    width: '80%',
-  },
-  goAnyway: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  button: {
-    border: 0,
-    backgroundColor: 'transparent',
-    color: 'black',
-    fontSize: 15,
-    padding: 6,
-    borderRadius: 5,
-    width: 200,
-    backgroundImage:
-      'linear-gradient(140deg,#ffffff 25%, #ffffff 50%,#ffffff 90%)',
-    transition: 'width 2s, background-image 4s, ease-out',
-    '&:hover': {
-      backgroundImage:
-        'linear-gradient(140deg,#faefe8 25%, #d7dae8 50%,#cedaa4 90%)',
-      width: 400,
-    },
-  },
-  link: {
-    color: 'black',
-    fontSize: 15,
-    borderRadius: 5,
-    padding: 6,
-    textAlign: 'center',
-    '&:hover': {
-      backgroundImage:
-        'linear-gradient(140deg,#faefe8 25%, #d7dae8 50%,#cedaa4 90%)',
-    },
-  },
+  root: { height: '100%', width: '80%', margin: '150px 0px 0px 150px' },
+  quote: {},
+  person: { marginTop: '30px' },
 }));
 
 const BlockError = (props) => {
-  console.log('block error props', props);
   const classes = useStyles();
-  const dispatch = useDispatch();
-  let history = useHistory();
-  //states
-  const blocks = useSelector((state) => state.blocks);
-  const auth = useSelector((state) => state.auth);
+  const [quoteToDisplay, setQuoteToDisplay] = useState('');
+  useEffect(() => {
+    const quotes = [
+      {
+        quote:
+          'All we have to decide is what to do with the time that is given us.',
+        person: 'J. R. R. Tolkien',
+      },
+      {
+        quote:
+          'If you love life, don’t waste time, for time is what life is made up of.',
+        person: 'Bruce Lee',
+      },
+      {
+        quote:
+          'If we take care of the moments, the years will take care of themselves.',
+        person: 'Maria Edgeworth',
+      },
+      {
+        quote:
+          'Yesterday’s the past, tomorrow’s the future, but today is a gift. That’s why it’s called the present.',
+        person: 'Bil Keane',
+      },
+    ];
+    const quoteToDisplayIdx = Math.floor(Math.random() * quotes.length);
+    console.log(quoteToDisplayIdx);
 
-  const latestBlockUrl = blocks?.[0]?.site?.siteUrl;
-  const latestBlockId = blocks?.[0]?.site?.id;
-
-  console.log('latest blocked url', latestBlockUrl);
-
-  const [answers, setAnswers] = useState({
-    question1: 'false',
-    question2: 'false',
-    question3: 'false',
-  });
-  const [showLink, setShowLink] = useState(false);
-
-  const goOrNoGo = () => {
-    if (
-      answers.question1 === 'true' &&
-      answers.question2 === 'true' &&
-      answers.question3 === 'true'
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  const goAnyway = () => {
-    dispatch(updateBlocking(auth.id, latestBlockId));
-    setShowLink(true);
-  };
-
-  const handleChange = (ev) => {
-    setAnswers({ ...answers, [ev.target.name]: ev.target.value });
-  };
-
-  // console.log(answers);
+    setQuoteToDisplay(quotes[quoteToDisplayIdx]);
+  }, []);
 
   return (
-    <div id="uhoh-blocked" className={classes.uhoh}>
-      <Typography variant="h5" gutterBottom>
-        If you love life, don’t waste time, for time is what life is made up of.
-        -Bruce Lee
-      </Typography>
-      <Typography variant="body1">
-        🧠 Think about these before you start browsing around:
-      </Typography>
-    </div>
+    <Grid
+      container
+      direction="column"
+      alignItems="flex-start"
+      className={classes.root}
+    >
+      <Grid item>
+        <Typography className={classes.quote} variant="h1">
+          {quoteToDisplay.quote}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography className={classes.person} variant="h1">
+          - {quoteToDisplay.person}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
