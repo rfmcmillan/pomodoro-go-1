d4836b9df265a3c5d7e6da7dfac971727773fd61
Russel McMillan <rfmcmillan@gmail.com>
2022-01-17 07:17:44 -0500

    started README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # PomodoroGo
+
+PomodoroGo is a Chrome extension that is a pomodoro timer (https://en.wikipedia.org/wiki/Pomodoro_Technique) and web-site blocker in one. It also provides a dashboard to help the user gain insights from their work or study session history.
+
+## Installation
+
+1. Within Chrome, navigate to https://chrome.google.com/webstore/detail/pomodorogo/hdkcidbbeignlpjolgfjelpkinmgkefd
+
+2. Click the 'Add to Chrome' button. Your browser should produce a dialog telling you that PomodoroGo can read your browsing history and display notifications. PomodoroGo uses browsing history to block websites and notifications to tell you when your session is complete.
+
+3. Click 'Add extension'. PomodoroGo should now be installed and available in your Chrome extensions.
+
+## Usage
+
+1. To launch PomodoroGo, click the Extensions button in the upper-right of your Chrome window and select 'PomodoroGo'.
+
+2. If you would like to explore the sandbox account, click the 'Sandbox' button. If you'd like to create your own account, click 'Sign Up'.
+
+3. To set a timer...
+
+    - Select a goal in the 'Select Goal' dropdown.
+    - Enter the desired length of your session.
+    - Click 'Start'. The rest of the app will now be hidden to help you focus. When the timer is complete you will receive a notification from Chrome.
+
+4. To block a website...
+    - In the Navbar, click 'BlockList'.
+    - In the 'URL' input, type the url of the website you would like to block. e.g. 'facebook.com' or 'https://www.facebook.com/'. Either will work.
+    - Select a category from the 'Category' dropdown menu.
--- client/components/Timer/Stopwatch.js
+++ client/components/Timer/Stopwatch.js
@@ -1,178 +1,180 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { Button, Typography, makeStyles, Card, Grid } from '@material-ui/core';
-import { useTheme } from '@material-ui/core/styles';
-import { connect, useDispatch, useSelector } from 'react-redux';
-import { updateSession } from '../../store/sessions';
-import StopButton from './StopButton';
-import { SessionContext } from '../../app';
-import { Circle } from 'rc-progress';
-import { endSession } from '../../store/sessions';
+import React, { useState, useContext, useEffect } from "react";
+import { Button, Typography, makeStyles, Card, Grid } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { updateSession } from "../../store/sessions";
+import StopButton from "./StopButton";
+import { SessionContext } from "../../app";
+import { Circle } from "rc-progress";
+import { endSession } from "../../store/sessions";
 
 const useStyles = makeStyles(() => ({
-  timerContainer: {
-    borderRadius: '15px',
-    height: '500px',
-    width: '500px',
-    margin: '35px',
-    padding: '10px',
-  },
-  timer: {
-    fontSize: '100px',
-  },
+    timerContainer: {
+        borderRadius: "15px",
+        height: "500px",
+        width: "500px",
+        margin: "35px",
+        padding: "10px",
+    },
+    timer: {
+        fontSize: "100px",
+    },
 }));
 
 const msToHMS = (ms) => {
-  let seconds = ms / 1000;
+    let seconds = ms / 1000;
 
-  let hours = parseInt(seconds / 3600);
-  seconds = seconds % 3600;
+    let hours = parseInt(seconds / 3600);
+    seconds = seconds % 3600;
 
-  let minutes = parseInt(seconds / 60);
-  seconds = seconds % 60;
+    let minutes = parseInt(seconds / 60);
+    seconds = seconds % 60;
 
-  hours = hours < 10 ? '0' + hours : hours;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? (seconds >= 0 ? '0' + seconds : '00') : seconds;
-  return hours + ':' + minutes + ':' + seconds;
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? (seconds >= 0 ? "0" + seconds : "00") : seconds;
+    return hours + ":" + minutes + ":" + seconds;
 };
 
 const Stopwatch = (props) => {
-  const { updateSession } = props;
-  const dispatch = useDispatch();
-  const { localIsActive, setLocalIsActive, sessionTime, setSessionTime } =
-    useContext(SessionContext);
-  const [timeLeft, setTimeLeft] = useState(0);
-  const displayTime = msToHMS(localIsActive ? timeLeft : sessionTime);
-  const classes = useStyles();
-  const theme = useTheme();
-  const { primary } = theme.palette;
-  const currentSession = useSelector((state) => state.currentSession);
-  const { expectedEndTime, startTime } = currentSession;
-  const end = Date.parse(expectedEndTime);
-  const start = Date.parse(startTime);
-  const [triggerEnd, setTriggerEnd] = useState(false);
-  let intervalId;
+    const { updateSession } = props;
+    const dispatch = useDispatch();
+    const { localIsActive, setLocalIsActive, sessionTime, setSessionTime } =
+        useContext(SessionContext);
+    const [timeLeft, setTimeLeft] = useState(0);
+    const displayTime = msToHMS(localIsActive ? timeLeft : sessionTime);
+    const classes = useStyles();
+    const theme = useTheme();
+    const { primary } = theme.palette;
+    const currentSession = useSelector((state) => state.currentSession);
+    const { expectedEndTime, startTime } = currentSession;
+    const end = Date.parse(expectedEndTime);
+    const start = Date.parse(startTime);
+    const [triggerEnd, setTriggerEnd] = useState(false);
+    let intervalId;
 
-  useEffect(() => {
-    chrome.runtime.sendMessage(
-      'nmfhcdkehekkflbopjlnnihncpnlejho',
-      { cmd: 'GET_TIME' },
-      (response) => {
-        if (response.time) {
-          const time = new Date(response.time);
-          startTimer(time);
+    useEffect(() => {
+        chrome.runtime.sendMessage(
+            "nmfhcdkehekkflbopjlnnihncpnlejho",
+            { cmd: "GET_TIME" },
+            (response) => {
+                if (response.time) {
+                    const time = new Date(response.time);
+                    startTimer(time);
+                }
+            }
+        );
+    }, []);
+
+    const startTimer = (time, sessionTime) => {
+        setTimeLeft(sessionTime);
+        intervalId = setInterval(() => {
+            if (time.getTime() >= Date.now()) {
+                setTimeLeft((prevTimeLeft) => prevTimeLeft - 1000);
+            } else {
+                setTimeLeft(0);
+                setTriggerEnd(true);
+            }
+        }, 1000);
+    };
+
+    useEffect(() => {
+        if (localIsActive) {
+            startTimerInit(sessionTime);
+            return () => clearInterval(intervalId);
         }
-      }
+    }, [localIsActive]);
+
+    useEffect(() => {
+        if (triggerEnd && localIsActive) {
+            dispatch(endSession(currentSession.id, true));
+            clearInterval(intervalId);
+            setLocalIsActive(false);
+            setTriggerEnd(false);
+        }
+    }, [triggerEnd]);
+
+    function startTimerInit(sessionTime) {
+        const now = Date.now();
+        const timeToFinish = now + sessionTime;
+        const timeDate = new Date(timeToFinish);
+        chrome.runtime.sendMessage("nmfhcdkehekkflbopjlnnihncpnlejho", {
+            cmd: "START_TIMER",
+            when: timeDate,
+        });
+        updateSession(currentSession.id, { sessionTime });
+        localStorage.setItem("currentSession", JSON.stringify(currentSession));
+        startTimer(timeDate, sessionTime);
+    }
+
+    return (
+        <div>
+            <Card className={classes.timerContainer} elevation={10}>
+                <Grid container direction="column" alignItems="center">
+                    <Grid item>
+                        <Typography
+                            variant="h1"
+                            className={classes.timer}
+                            style={{
+                                position: "relative",
+                                top: "185px",
+                            }}
+                        >
+                            {displayTime}{" "}
+                        </Typography>
+                    </Grid>
+                    {localIsActive ? (
+                        <StopButton intervalId={intervalId} />
+                    ) : (
+                        <Button
+                            onClick={() => {
+                                setLocalIsActive(true);
+                            }}
+                            disabled={sessionTime ? false : true}
+                            style={{
+                                backgroundColor: "#5061a9",
+                                color: "white",
+                                marginLeft: "4px",
+                                marginBottom: "10px",
+                                zIndex: 1,
+                                position: "relative",
+                                top: "185px",
+                            }}
+                        >
+                            Start
+                        </Button>
+                    )}
+                </Grid>
+                <Circle
+                    percent={
+                        ((localIsActive ? timeLeft : sessionTime) /
+                            sessionTime) *
+                        100
+                    }
+                    strokeWidth="3"
+                    strokeColor={{
+                        "0%": "#5061a9",
+                        "100%": "#5061a9",
+                    }}
+                    trailColor={primary.contrastText}
+                    style={{
+                        width: "100%",
+                        position: "relative",
+                        bottom: "160px",
+                    }}
+                />
+            </Card>
+        </div>
     );
-  }, []);
-
-  const startTimer = (time, sessionTime) => {
-    setTimeLeft(sessionTime);
-    intervalId = setInterval(() => {
-      if (time.getTime() >= Date.now()) {
-        setTimeLeft((prevTimeLeft) => prevTimeLeft - 1000);
-      } else {
-        setTimeLeft(0);
-        setTriggerEnd(true);
-      }
-    }, 1000);
-  };
-
-  useEffect(() => {
-    if (localIsActive) {
-      startTimerInit(sessionTime);
-      return () => clearInterval(intervalId);
-    }
-  }, [localIsActive]);
-
-  useEffect(() => {
-    if (triggerEnd && localIsActive) {
-      dispatch(endSession(currentSession.id, true));
-      clearInterval(intervalId);
-      setLocalIsActive(false);
-      setTriggerEnd(false);
-    }
-  }, [triggerEnd]);
-
-  function startTimerInit(sessionTime) {
-    const now = Date.now();
-    const timeToFinish = now + sessionTime;
-    const timeDate = new Date(timeToFinish);
-    chrome.runtime.sendMessage('nmfhcdkehekkflbopjlnnihncpnlejho', {
-      cmd: 'START_TIMER',
-      when: timeDate,
-    });
-    updateSession(currentSession.id, { sessionTime });
-    localStorage.setItem('currentSession', JSON.stringify(currentSession));
-    startTimer(timeDate, sessionTime);
-  }
-
-  return (
-    <div>
-      <Card className={classes.timerContainer} elevation={10}>
-        <Grid container direction="column" alignItems="center">
-          <Grid item>
-            <Typography
-              variant="h1"
-              className={classes.timer}
-              style={{
-                position: 'relative',
-                top: '185px',
-              }}
-            >
-              {displayTime}{' '}
-            </Typography>
-          </Grid>
-          {localIsActive ? (
-            <StopButton intervalId={intervalId} />
-          ) : (
-            <Button
-              onClick={() => {
-                setLocalIsActive(true);
-              }}
-              disabled={sessionTime ? false : true}
-              style={{
-                backgroundColor: '#5061a9',
-                color: 'white',
-                marginLeft: '4px',
-                marginBottom: '10px',
-                zIndex: 1,
-                position: 'relative',
-                top: '185px',
-              }}
-            >
-              Start
-            </Button>
-          )}
-        </Grid>
-        <Circle
-          percent={
-            ((localIsActive ? timeLeft : sessionTime) / sessionTime) * 100
-          }
-          strokeWidth="3"
-          strokeColor={{
-            '0%': '#5061a9',
-            '100%': '#5061a9',
-          }}
-          trailColor={primary.contrastText}
-          style={{
-            width: '100%',
-            position: 'relative',
-            bottom: '160px',
-          }}
-        />
-      </Card>
-    </div>
-  );
 };
 export default connect(
-  (state) => state,
-  (dispatch) => {
-    return {
-      updateSession: (sessionId, sessionTime) =>
-        dispatch(updateSession(sessionId, sessionTime)),
-    };
-  }
+    (state) => state,
+    (dispatch) => {
+        return {
+            updateSession: (sessionId, sessionTime) =>
+                dispatch(updateSession(sessionId, sessionTime)),
+        };
+    }
 )(Stopwatch);
 
 export { msToHMS };
