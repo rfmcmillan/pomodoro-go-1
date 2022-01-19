4c4c99c04926c56ae859a4b0885d6152d5bc89bc
Russel McMillan <rfmcmillan@gmail.com>
2022-01-19 09:01:27 -0500

    removed Explore UI button

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -1,78 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { Grid, Typography, Fade, Button, Tooltip } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Grid, Typography, Fade, Button, Tooltip } from "@material-ui/core";
 
 const Home = (props) => {
-  const theme = useTheme();
-  const [browserIsChrome, setBrowserIsChrome] = useState(true);
-  const useStyles = makeStyles((theme) => ({
-    button: {
-      borderRadius: 30,
-      backgroundColor: 'black',
-      color: 'white',
-      textTransform: 'capitalize',
-      fontWeight: 100,
-      marginTop: 30,
-      boxShadow: '0px 10px 10px #d3aa93',
-      height: 45,
-      fontSize: 16,
-    },
-    description: { paddingRight: 100, paddingBottom: 300 },
-    exploreButton: {
-      borderRadius: 30,
-      backgroundColor: 'black',
-      color: 'white',
-      textTransform: 'capitalize',
-      fontWeight: 100,
-      margin: '30px 0px 0px 20px',
-      boxShadow: '0px 10px 10px #d3aa93',
-      height: 45,
-      fontSize: 16,
-    },
-    exploreTooltip: { fonstSize: 20 },
-    image: { width: '100%' },
-    root: { width: '90%', height: '90vh' },
-    subTitle: { fontSize: 20, fontWeight: 400 },
-    title: { fontSize: 40, fontWeight: 700 },
-  }));
-  const classes = useStyles();
+    const theme = useTheme();
+    const [browserIsChrome, setBrowserIsChrome] = useState(true);
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            borderRadius: 30,
+            backgroundColor: "black",
+            color: "white",
+            textTransform: "capitalize",
+            fontWeight: 100,
+            marginTop: 30,
+            boxShadow: "0px 10px 10px #d3aa93",
+            height: 45,
+            fontSize: 16,
+        },
+        description: { paddingRight: 100, paddingBottom: 300 },
+        exploreButton: {
+            borderRadius: 30,
+            backgroundColor: "black",
+            color: "white",
+            textTransform: "capitalize",
+            fontWeight: 100,
+            margin: "30px 0px 0px 20px",
+            boxShadow: "0px 10px 10px #d3aa93",
+            height: 45,
+            fontSize: 16,
+        },
+        exploreTooltip: { fonstSize: 20 },
+        image: { width: "100%" },
+        root: { width: "90%", height: "90vh" },
+        subTitle: { fontSize: 20, fontWeight: 400 },
+        title: { fontSize: 40, fontWeight: 700 },
+    }));
+    const classes = useStyles();
 
-  useEffect(() => {
-    const indexOfChrome = navigator.userAgent.indexOf('Chrome');
-    if (indexOfChrome < 0) {
-      setBrowserIsChrome(false);
-    }
-  }, []);
+    useEffect(() => {
+        const indexOfChrome = navigator.userAgent.indexOf("Chrome");
+        if (indexOfChrome < 0) {
+            setBrowserIsChrome(false);
+        }
+    }, []);
 
-  return (
-    <Fade easing="ease-in" timeout={1000} in={true}>
-      <Grid container alignItems="center" direction="column">
-        <Grid
-          item
-          container
-          className={classes.root}
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Grid className={classes.description} item xs={6}>
-            <Typography variant="h1" className={classes.title}>
-              Stay focused and track your productivity
-            </Typography>
-            <Typography variant="h2" className={classes.subTitle}>
-              A pomodoro timer and website blocker for Chrome that gives you
-              insights into your productivity
-            </Typography>
+    return (
+        <Fade easing="ease-in" timeout={1000} in={true}>
+            <Grid container alignItems="center" direction="column">
+                <Grid
+                    item
+                    container
+                    className={classes.root}
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <Grid className={classes.description} item xs={6}>
+                        <Typography variant="h1" className={classes.title}>
+                            Stay focused and track your productivity
+                        </Typography>
+                        <Typography variant="h2" className={classes.subTitle}>
+                            A pomodoro timer and website blocker for Chrome that
+                            gives you insights into your productivity
+                        </Typography>
 
-            <Button
-              className={classes.button}
-              variant="contained"
-              type="submit"
-              href="https://chrome.google.com/webstore/detail/pomodorogo/hdkcidbbeignlpjolgfjelpkinmgkefd"
-            >
-              Download Chrome Extension
-            </Button>
+                        <Button
+                            className={classes.button}
+                            variant="contained"
+                            type="submit"
+                            href="https://chrome.google.com/webstore/detail/pomodorogo/hdkcidbbeignlpjolgfjelpkinmgkefd"
+                        >
+                            Download Chrome Extension
+                        </Button>
 
-            {browserIsChrome ? (
+                        {/* {browserIsChrome ? (
               <Tooltip
                 className={classes.exploreTooltip}
                 title="If you can't download the extension, you can explore the user interface by clicking here. The only thing that won't be activated is the website-blocker."
@@ -88,15 +88,18 @@ const Home = (props) => {
               </Tooltip>
             ) : (
               ''
-            )}
-          </Grid>
-          <Grid className={classes.stack} item xs={6}>
-            <img className={classes.image} src="./assets/Home.png" />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Fade>
-  );
+            )} */}
+                    </Grid>
+                    <Grid className={classes.stack} item xs={6}>
+                        <img
+                            className={classes.image}
+                            src="./assets/Home.png"
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Fade>
+    );
 };
 
 export default Home;
--- client/routes.js
+++ client/routes.js
@@ -1,111 +1,115 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Login, Signup } from './components/AuthForm';
-import SandboxLogin from './components/AuthForm/SandboxLogin';
-import CreateSession from './components/Timer/CreateSession';
-import Timer from './components/Timer';
-import Dashboard from './components/Dashboard';
-import { loadSessions } from './store/sessions';
-import { loadBlackLists, updateBlackList } from './store/blackList';
-import { loadBlocks } from './store/blocks';
-import { loadSites } from './store/sites';
-import { getSites } from './store/blockSites';
-import BlockError from './components/BlockError';
-import BlockSites from './components/BlockSites';
-import Friends from './components/Friends/Friends';
-import About from './components/About';
-import Home from './components/Home';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Login, Signup } from "./components/AuthForm";
+import SandboxLogin from "./components/AuthForm/SandboxLogin";
+import CreateSession from "./components/Timer/CreateSession";
+import Timer from "./components/Timer";
+import Dashboard from "./components/Dashboard";
+import { loadSessions } from "./store/sessions";
+import { loadBlackLists, updateBlackList } from "./store/blackList";
+import { loadBlocks } from "./store/blocks";
+import { loadSites } from "./store/sites";
+import { getSites } from "./store/blockSites";
+import BlockError from "./components/BlockError";
+import BlockSites from "./components/BlockSites";
+import Friends from "./components/Friends/Friends";
+import About from "./components/About";
+import Home from "./components/Home";
 
 class Routes extends Component {
-  constructor(props) {
-    super(props);
-  }
-  async componentDidMount() {
-    await this.props.loadInitialData();
-  }
-  async componentDidUpdate(prevProps) {
-    if (this.props.auth && this.props.auth.id !== prevProps.auth.id) {
-      await this.props.getSites(this.props.auth.id);
+    constructor(props) {
+        super(props);
     }
-    chrome?.runtime?.sendMessage('nmfhcdkehekkflbopjlnnihncpnlejho', {
-      message: 'set-blocked-sites',
+    async componentDidMount() {
+        await this.props.loadInitialData();
+    }
+    async componentDidUpdate(prevProps) {
+        if (this.props.auth && this.props.auth.id !== prevProps.auth.id) {
+            await this.props.getSites(this.props.auth.id);
+        }
+        chrome?.runtime?.sendMessage("nmfhcdkehekkflbopjlnnihncpnlejho", {
+            message: "set-blocked-sites",
 
-      blockedSites: this.props.blockedSites.filter((each) => {
-        return each.blacklist.blockingEnabled === true;
-      }),
-      currUser: this.props.auth.id,
-    });
-  }
+            blockedSites: this.props.blockedSites.filter((each) => {
+                return each.blacklist.blockingEnabled === true;
+            }),
+            currUser: this.props.auth.id,
+        });
+    }
 
-  render() {
-    const { isLoggedIn, auth, blackList, updateB, timer } = this.props;
+    render() {
+        const { isLoggedIn, auth, blackList, updateB, timer } = this.props;
 
-    return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/timer">
-              <Timer timer={timer} />
-            </Route>
-            <Route path="/login">
-              <Redirect to="/timer" />
-            </Route>
-            <Route path="/sandboxLogin">
-              <Redirect to="/timer" />
-            </Route>
-            <Route path="/" exact component={Timer} />
-            <Route path="/home" exact component={Home} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/timer" exact>
-              <CreateSession />
-            </Route>
-            <Route exact path="/blocksites" component={BlockSites} />
-            <Route exact path="/friends" component={Friends} />
-            <Route exact path="/blocked" component={BlockError} />
-            <Route path="/about" component={About} />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/home" exact component={Home} />
-            <Route path="/sandboxLogin" component={SandboxLogin} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route exact path="/blocked" component={BlockError} />
-          </Switch>
-        )}
-      </div>
-    );
-  }
+        return (
+            <div>
+                {isLoggedIn ? (
+                    <Switch>
+                        <Route path="/timer">
+                            <Timer timer={timer} />
+                        </Route>
+                        <Route path="/login">
+                            <Redirect to="/dashboard" />
+                        </Route>
+                        <Route path="/sandboxLogin">
+                            <Redirect to="/dashboard" />
+                        </Route>
+                        <Route path="/" exact component={Timer} />
+                        <Route path="/home" exact component={Home} />
+                        <Route path="/dashboard" component={Dashboard} />
+                        <Route path="/timer" exact>
+                            <CreateSession />
+                        </Route>
+                        <Route
+                            exact
+                            path="/blocksites"
+                            component={BlockSites}
+                        />
+                        <Route exact path="/friends" component={Friends} />
+                        <Route exact path="/blocked" component={BlockError} />
+                        <Route path="/about" component={About} />
+                    </Switch>
+                ) : (
+                    <Switch>
+                        <Route path="/about" component={About} />
+                        <Route path="/home" exact component={Home} />
+                        <Route path="/sandboxLogin" component={SandboxLogin} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/signup" component={Signup} />
+                        <Route exact path="/blocked" component={BlockError} />
+                    </Switch>
+                )}
+            </div>
+        );
+    }
 }
 
 const mapState = (state) => {
-  return {
-    isLoggedIn: !!state.auth.id,
-    sites: state.sites,
-    auth: state.auth,
-    blackList: state.blackList,
-    blockedSites: state.blockedSites,
-  };
+    return {
+        isLoggedIn: !!state.auth.id,
+        sites: state.sites,
+        auth: state.auth,
+        blackList: state.blackList,
+        blockedSites: state.blockedSites,
+    };
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(loadSessions());
-      dispatch(loadSites());
-      dispatch(loadBlocks());
-    },
+    return {
+        loadInitialData() {
+            dispatch(loadSessions());
+            dispatch(loadSites());
+            dispatch(loadBlocks());
+        },
 
-    updateB: (blackListId, blackListInfo) => {
-      return dispatch(updateBlackList(blackListId, blackListInfo));
-    },
+        updateB: (blackListId, blackListInfo) => {
+            return dispatch(updateBlackList(blackListId, blackListInfo));
+        },
 
-    getSites: (userId) => {
-      return dispatch(getSites(userId));
-    },
-  };
+        getSites: (userId) => {
+            return dispatch(getSites(userId));
+        },
+    };
 };
 
 export default withRouter(connect(mapState, mapDispatch)(Routes));
