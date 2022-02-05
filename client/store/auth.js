d2bd6a2c43a7c74c917ae4741072523b5a0db197
Russel McMillan <rfmcmillan@gmail.com>
2022-02-05 14:17:15 -0500

    installed prettier

--- client/store/auth.js
+++ client/store/auth.js
@@ -1,78 +1,78 @@
-import axios from "axios";
-import history from "../history";
-import socketIOClient from "socket.io-client";
+import axios from "axios"
+import history from "../history"
+import socketIOClient from "socket.io-client"
 
-const ENDPOINT = process.env.API_URL;
-export const socket = socketIOClient(ENDPOINT);
+const ENDPOINT = process.env.API_URL
+export const socket = socketIOClient(ENDPOINT)
 
-const TOKEN = "token";
+const TOKEN = "token"
 
-const SET_AUTH = "SET_AUTH";
+const SET_AUTH = "SET_AUTH"
 
-const setAuth = (auth) => ({ type: SET_AUTH, auth });
+const setAuth = (auth) => ({ type: SET_AUTH, auth })
 
 export const me = () => async (dispatch) => {
-    const token = window.localStorage.getItem(TOKEN);
-    if (token) {
-        const res = await axios.get(`${process.env.API_URL}/auth/me`, {
-            headers: {
-                authorization: token,
-            },
-        });
-        return dispatch(setAuth(res.data));
-    }
-};
+  const token = window.localStorage.getItem(TOKEN)
+  if (token) {
+    const res = await axios.get(`${process.env.API_URL}/auth/me`, {
+      headers: {
+        authorization: token,
+      },
+    })
+    return dispatch(setAuth(res.data))
+  }
+}
 export const authenticateGoogle =
-    (data = {}) =>
-    async (dispatch) => {
-        try {
-            const response = await axios.post("/auth/google", data, {
-                headers: { authorization: data.tokenId },
-            });
-            window.localStorage.setItem("token", response.data.token);
-            dispatch(me());
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  (data = {}) =>
+  async (dispatch) => {
+    try {
+      const response = await axios.post("/auth/google", data, {
+        headers: { authorization: data.tokenId },
+      })
+      window.localStorage.setItem("token", response.data.token)
+      dispatch(me())
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 export const authenticate =
-    (username, email, password, method) => async (dispatch) => {
-        try {
-            const res = await axios.post(
-                `${process.env.API_URL}/auth/${method}`,
-                method === "signup"
-                    ? { username, email, password }
-                    : { email, password }
-            );
-            window.localStorage.setItem(TOKEN, res.data.token);
-            dispatch(me());
-        } catch (authError) {
-            return dispatch(setAuth({ error: authError }));
-        }
-    };
+  (username, email, password, method) => async (dispatch) => {
+    try {
+      const res = await axios.post(
+        `${process.env.API_URL}/auth/${method}`,
+        method === "signup"
+          ? { username, email, password }
+          : { email, password }
+      )
+      window.localStorage.setItem(TOKEN, res.data.token)
+      dispatch(me())
+    } catch (authError) {
+      return dispatch(setAuth({ error: authError }))
+    }
+  }
 
 export const logout = () => {
-    window.localStorage.removeItem(TOKEN);
-    window.localStorage.removeItem("user");
-    window.localStorage.removeItem("timerDone");
-    history.push("/login");
-    return {
-        type: SET_AUTH,
-        auth: {},
-    };
-};
+  window.localStorage.removeItem(TOKEN)
+  window.localStorage.removeItem("user")
+  window.localStorage.removeItem("timerDone")
+  history.push("/login")
+  return {
+    type: SET_AUTH,
+    auth: {},
+  }
+}
 
 export default function (state = {}, action) {
-    switch (action.type) {
-        case SET_AUTH:
-            if (action.auth.id) {
-                socket.emit("login", { userId: action.auth.id });
-            } else {
-                socket.emit("logout", { userId: state.id });
-            }
-            return action.auth;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_AUTH:
+      if (action.auth.id) {
+        socket.emit("login", { userId: action.auth.id })
+      } else {
+        socket.emit("logout", { userId: state.id })
+      }
+      return action.auth
+    default:
+      return state
+  }
 }
--- client/store/blockSites.js
+++ client/store/blockSites.js
@@ -1,96 +1,94 @@
-import axios from "axios";
-import { createBlackList } from "./blackList";
+import axios from "axios"
+import { createBlackList } from "./blackList"
 
-const GET_SITES = "GET_SITES";
-const ADD_SITE = "ADD_SITE";
+const GET_SITES = "GET_SITES"
+const ADD_SITE = "ADD_SITE"
 
 export const getSites = (userId) => {
-    return async (dispatch) => {
-        try {
-            const currentUser = (
-                await axios.get(`${process.env.API_URL}/api/users/${userId}`)
-            ).data;
-            const blockedSites = currentUser.sites;
-            dispatch(_getSites(blockedSites));
-        } catch (err) {
-            console.log(err);
-        }
-    };
-};
+  return async (dispatch) => {
+    try {
+      const currentUser = (
+        await axios.get(`${process.env.API_URL}/api/users/${userId}`)
+      ).data
+      const blockedSites = currentUser.sites
+      dispatch(_getSites(blockedSites))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
 
 const _getSites = (blockedSites) => {
-    return {
-        type: GET_SITES,
-        blockedSites,
-    };
-};
+  return {
+    type: GET_SITES,
+    blockedSites,
+  }
+}
 
 const _addSite = (blockedSite) => {
-    return {
-        type: ADD_SITE,
-        blockedSite,
-    };
-};
+  return {
+    type: ADD_SITE,
+    blockedSite,
+  }
+}
 
 export const addSite = (site, userId) => {
-    return async (dispatch) => {
-        try {
-            const newSite = (
-                await axios.post(`${process.env.API_URL}/api/sites`, {
-                    ...site,
-                    userId: userId,
-                })
-            ).data;
-            const newBlackList = await axios.post(
-                `${process.env.API_URL}/api/blackList`,
-                {
-                    siteId: newSite.id,
-                    userId,
-                }
-            ).data;
-            const { siteId, userId } = newBlackList;
-            dispatch(_addSite(newSite));
-            createBlackList(siteId, userId);
-        } catch (err) {
-            console.log(err);
+  return async (dispatch) => {
+    try {
+      const newSite = (
+        await axios.post(`${process.env.API_URL}/api/sites`, {
+          ...site,
+          userId: userId,
+        })
+      ).data
+      const newBlackList = await axios.post(
+        `${process.env.API_URL}/api/blackList`,
+        {
+          siteId: newSite.id,
+          userId,
         }
-    };
-};
+      ).data
+      const { siteId, userId } = newBlackList
+      dispatch(_addSite(newSite))
+      createBlackList(siteId, userId)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
 
 export const deleteSite = (userId, siteId) => {
-    return async (dispatch) => {
-        try {
-            await axios.delete(
-                `${process.env.API_URL}/api/sites/${userId}/${siteId}`
-            );
-            dispatch(getSites(userId));
-        } catch (err) {
-            console.log(err);
-        }
-    };
-};
+  return async (dispatch) => {
+    try {
+      await axios.delete(`${process.env.API_URL}/api/sites/${userId}/${siteId}`)
+      dispatch(getSites(userId))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
 
 export const updateBlocking = (userId, siteId) => {
-    return async (dispatch) => {
-        try {
-            await axios.put(
-                `${process.env.API_URL}/api/blackList/${userId}/${siteId}`
-            );
-            dispatch(getSites(userId));
-        } catch (err) {
-            console.log(err);
-        }
-    };
-};
+  return async (dispatch) => {
+    try {
+      await axios.put(
+        `${process.env.API_URL}/api/blackList/${userId}/${siteId}`
+      )
+      dispatch(getSites(userId))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
 
 const blockedSitesReducer = (state = [], action) => {
-    if (action.type === GET_SITES) {
-        return (state = action.blockedSites);
-    }
-    if (action.type === ADD_SITE) {
-        return (state = [...state, action.blockedSite]);
-    }
-    return state;
-};
+  if (action.type === GET_SITES) {
+    return (state = action.blockedSites)
+  }
+  if (action.type === ADD_SITE) {
+    return (state = [...state, action.blockedSite])
+  }
+  return state
+}
 
-export default blockedSitesReducer;
+export default blockedSitesReducer
