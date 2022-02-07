cfc89b3abcefe51b41636fb5691fdb0c35bdc032
Russel McMillan <rfmcmillan@gmail.com>
2022-02-07 06:42:32 -0500

    added console.errors to blockSites thunks

--- client/store/blockSites.js
+++ client/store/blockSites.js
@@ -1,36 +1,36 @@
-import axios from "axios"
-import { createBlackList } from "./blackList"
+import axios from "axios";
+import { createBlackList } from "./blackList";
 
-const GET_SITES = "GET_SITES"
-const ADD_SITE = "ADD_SITE"
+const GET_SITES = "GET_SITES";
+const ADD_SITE = "ADD_SITE";
 
 export const getSites = (userId) => {
   return async (dispatch) => {
     try {
       const currentUser = (
         await axios.get(`${process.env.API_URL}/api/users/${userId}`)
-      ).data
-      const blockedSites = currentUser.sites
-      dispatch(_getSites(blockedSites))
+      ).data;
+      const blockedSites = currentUser.sites;
+      dispatch(_getSites(blockedSites));
     } catch (err) {
-      console.log(err)
+      console.error(err);
     }
-  }
-}
+  };
+};
 
 const _getSites = (blockedSites) => {
   return {
     type: GET_SITES,
     blockedSites,
-  }
-}
+  };
+};
 
 const _addSite = (blockedSite) => {
   return {
     type: ADD_SITE,
     blockedSite,
-  }
-}
+  };
+};
 
 export const addSite = (site, userId) => {
   return async (dispatch) => {
@@ -40,55 +40,57 @@ export const addSite = (site, userId) => {
           ...site,
           userId: userId,
         })
-      ).data
+      ).data;
       const newBlackList = await axios.post(
         `${process.env.API_URL}/api/blackList`,
         {
           siteId: newSite.id,
           userId,
         }
-      ).data
-      const { siteId, userId } = newBlackList
-      dispatch(_addSite(newSite))
-      createBlackList(siteId, userId)
+      ).data;
+      const { siteId, userId } = newBlackList;
+      dispatch(_addSite(newSite));
+      createBlackList(siteId, userId);
     } catch (err) {
-      console.log(err)
+      console.error(err);
     }
-  }
-}
+  };
+};
 
 export const deleteSite = (userId, siteId) => {
   return async (dispatch) => {
     try {
-      await axios.delete(`${process.env.API_URL}/api/sites/${userId}/${siteId}`)
-      dispatch(getSites(userId))
+      await axios.delete(
+        `${process.env.API_URL}/api/sites/${userId}/${siteId}`
+      );
+      dispatch(getSites(userId));
     } catch (err) {
-      console.log(err)
+      console.error(err);
     }
-  }
-}
+  };
+};
 
 export const updateBlocking = (userId, siteId) => {
   return async (dispatch) => {
     try {
       await axios.put(
         `${process.env.API_URL}/api/blackList/${userId}/${siteId}`
-      )
-      dispatch(getSites(userId))
+      );
+      dispatch(getSites(userId));
     } catch (err) {
-      console.log(err)
+      console.error(err);
     }
-  }
-}
+  };
+};
 
 const blockedSitesReducer = (state = [], action) => {
   if (action.type === GET_SITES) {
-    return (state = action.blockedSites)
+    return (state = action.blockedSites);
   }
   if (action.type === ADD_SITE) {
-    return (state = [...state, action.blockedSite])
+    return (state = [...state, action.blockedSite]);
   }
-  return state
-}
+  return state;
+};
 
-export default blockedSitesReducer
+export default blockedSitesReducer;
